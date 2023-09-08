eb996224be565f4010febeeb1ed4c9a9a932f94e
Nagakumar2402 <nagakumar2402@gmail.com>
2023-09-08 21:00:27 +0530

    some chnages

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { FooterOne } from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-[#FFFFFF]">
       <Navbar />
       <HeroBanner />
       <Service />

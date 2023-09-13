c569714c89670f7ae3dadb6e84c34b5a3b754db6
Nagakumar2402 <nagakumar2402@gmail.com>
2023-09-14 02:21:28 +0530

    git add popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { Route, Routes,useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Location from "./components/Location";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
-import PopEvent from "./components/PopUPEvent";
 
 const App = () => {
-  const location = useLocation()
+  const location = useLocation();
   return (
-    
-      <div>
-        <Navbar />
-        <Routes location={location} key={location.pathname}>
-          <Route path="my-test/" element={<HomePage />} />
-          <Route path="my-test/about" element={<About />} />
-          <Route path="my-test/location" element={<Location />} />
-          <Route path="my-test/contact" element={<Contact/>} />
-          <Route path="my-test/:id" element={<PopEvent />} />
-        </Routes>
-      
-      </div>
-  
+    <div>
+      <Navbar />
+      <Routes location={location} key={location.pathname}>
+        <Route path="my-test/" element={<HomePage />} />
+        <Route path="my-test/about" element={<About />} />
+        <Route path="my-test/location" element={<Location />} />
+        <Route path="my-test/contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,9 +1,30 @@
 import React, { useState, useEffect } from "react";
 import lists from "../utils/all";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 const Events = () => {
+  const showSweetAlert = (imageUrl) => {
+    Swal.fire({
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      imageUrl: imageUrl,
 
-console.log(lists)
+      imageHeight: 630,
+      imageWidth: 1000,
+      imageAlt: "A tall image",
+      backdrop: true,
+      backgroundColor: "rgba(0,0,0,0.5) rgba(0, 0, 0, 0.808) ",
+      padding: "0px",
+      margin: "0",
+      confirmButtonColor: "green",
+
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  };
+
   return (
     <div className="pt-28 md:pt-5">
       <div
@@ -27,28 +48,26 @@ console.log(lists)
       </div>
       <div className="grid items-center w-full px-10 py-10 mx-auto space-y-4 max-w-7xl lg:px-20 lg:py-20 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
         {lists.map((list) => (
-          <Link
-          to={`/my-test/${list.id}`}
+          <div
             key={list.id}
-            
-            className="relative aspect-[16/9]  w-auto rounded-md md:aspect-auto md:h-[400px]"
+            className="relative aspect-[16/9]  w-auto rounded-md md:aspect-auto md:h-[400px] cursor-pointer"
+            onClick={() => showSweetAlert(list.image)}
           >
             <img
               src={list.image}
               alt={list.name}
-              className="z-0 object-cover w-full h-full rounded-md"
+              className="z-0 object-cover w-full h-full rounded-md responsive-image "
             />
             <div className="absolute inset-0 rounded-md bg-gradient-to-t from-gray-900 to-transparent"></div>
             <div className="absolute bottom-4 left-4">
               <h1 className="text-xl font-semibold text-white ">{list.name}</h1>
-              <button className="flex py-3 mt-4 text-sm font-semibold text-white cursor-pointer md:pl-48 text-end">
+              <button className="flex py-3 mt-4 text-sm font-semibold text-white cursor-pointer md:pl-40 text-end">
                 More &rarr;
               </button>
             </div>
-          </Link>
+          </div>
         ))}
       </div>
-      
     </div>
   );
 };

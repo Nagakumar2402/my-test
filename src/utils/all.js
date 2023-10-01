c07a61f656b91444828b050c897f66fef52069d8
Nagakumar2402 <nagakumar2402@gmail.com>
2023-10-01 18:09:58 +0530

    images

--- src/utils/all.js
+++ src/utils/all.js
@@ -1,34 +1,34 @@
-import birthday from "../assets/images/birthday.svg"
-import education from "../assets/images/education.svg"
-import FEEDING from "../assets/images/FEEDING.svg"
-import interaction from "../assets/images/interaction.svg"
+import birthday from "../assets/images/birthday.svg";
+import education from "../assets/images/education.svg";
+import FEEDING from "../assets/images/FEEDING.png";
+import interaction from "../assets/images/interaction.svg";
 
 const all = [
   {
     id: 1,
     name: "Birthday party's ",
-    image:birthday,
+    image: birthday,
     description:
       "We specialize in hosting birthday parties that are both entertaining and educational. Kids can enjoy hands-on animal interactions while celebrating their special day in a safe and supervised environment.",
   },
   {
     id: 2,
     name: "Education tours",
-    image:education,
+    image: education,
     description:
       " Our guided tours provide an opportunity for children and adults alike to learn about animals' behavior, life cycles, and fundamental needs. It's a fun and educational experience that fosters a deeper understanding of the animal world.",
   },
   {
     id: 3,
     name: "Animals Interactions ",
-    image:interaction,
+    image: interaction,
     description:
       "Guests can feed and pet a diverse selection of domesticated animals, creating unforgettable moments of connection between humans and animals.",
   },
   {
     id: 4,
     name: "Farming",
-    image:FEEDING,
+    image: FEEDING,
     description:
       "We offer opportunities for guests to experience the joys of farming, such as planting and harvesting crops, caring for livestock, and learning about sustainable agricultural practices. These activities provide a hands-on understanding of where our food comes from and the importance of responsible farming.",
   },

e05c84f766c3a70312c828b6b9ce54448e499a70
nagakumar2402 <nagakumar2402@gmail.com`>
2023-10-02 16:43:34 +0530

    added 234

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React from 'react'
 
-const Entry = () => {
+const Entry = ()=> {
   return (
-    <section className="py-32 md:py-20">
-      <div className="max-w-full flex flex-col items-center justify-center w-full px-4 mx-auto">
-        <div className="max-w-2xl mx-auto mb-16 text-center">
+    <div className="mx-auto max-w-7xl px-2 py-20 lg:px-8">
+      <div className="max-w-3xl mx-auto mb-16 text-center">
           <span className="font-bold tracking-widest text-green-700 uppercase">
             Pricing
           </span>
@@ -12,42 +11,80 @@ const Entry = () => {
             Zootopia Ticket Price
           </h2>
         </div>
-        <div className="grid w-full  grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-          <div className="p-6 space-y-6  text-gray-900 bg-[#c8b4ba] rounded shadow sm:p-8 hover:scale-105 ">
-            <div className="space-y-2">
-              <h4 className="text-2xl font-bold">Zootopia</h4>
-              <span className="text-2xl  font-bold md:text-3xl">
-                Entry charges
-              </span>
-            </div>
-            <div className="inline-block  w-full px-5 py-1 text-xl font-semibold text-center border-2 border-gray-700 rounded md:text-3xl bg-img">
-              &#8377; 499 Rs
-            </div>
+      <div className="grid grid-cols-1 gap-y-8 text-center sm:grid-cols-2 sm:gap-12 lg:grid-cols-3">
+        <div>
+          <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-blue-100">
+            <svg
+              className="h-9 w-9 text-blue-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+                d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
+              />
+            </svg>
           </div>
-          <div className="p-6 space-y-6   text-gray-900 bg-[#c8b4ba] rounded shadow sm:p-8 hover:scale-105">
-            <div className="space-y-2">
-              <h4 className="text-2xl  font-bold">animals</h4>
-              <span className="text-2xl font-bold md:text-3xl">Treat feed</span>
-            </div>
-            <div className="inline-block w-full px-5 py-1 text-xl font-semibold text-center border-2 border-gray-700 rounded md:text-3xl bg-img">
-              &#8377; 1250 Rs
-            </div>
+          <h3 className="mt-8 text-lg font-semibold text-black">Secured Payments</h3>
+          <p className="mt-4 text-sm text-gray-600">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            consequat duis enim velit mollit.
+          </p>
+        </div>
+       
+        <div>
+          <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-green-100">
+            <svg
+              className="h-9 w-9 text-green-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+              />
+            </svg>
           </div>
-          <div className="p-6 space-y-6  text-gray-900 bg-[#c8b4ba] rounded shadow-2xl  hover:scale-105 sm:p-8">
-            <div className="space-y-2">
-              <h4 className="text-2xl font-bold">animals</h4>
-              <span className="text-2xl  font-bold md:text-3xl">
-                Feeding charges
-              </span>
-            </div>
-            <div className="inline-block w-full px-5 py-1 text-xl font-semibold text-center border-2 border-gray-700 rounded md:text-3xl bg-img">
-              &#8377; 250 Rs
-            </div>
+          <h3 className="mt-8 text-lg font-semibold text-black">Light & Dark Version</h3>
+          <p className="mt-4 text-sm text-gray-600">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            consequat duis enim velit mollit.
+          </p>
+        </div>
+        <div>
+          <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-red-100">
+            <svg
+              className="h-9 w-9 text-red-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+                d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
+              />
+            </svg>
           </div>
+          <h3 className="mt-8 text-lg font-semibold text-black">Filter Blocks</h3>
+          <p className="mt-4 text-sm text-gray-600">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            consequat duis enim velit mollit.
+          </p>
         </div>
       </div>
-    </section>
-  );
-};
+    </div>
+  )
+}
 
-export default Entry;
+export default Entry

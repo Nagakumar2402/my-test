109361a62f6b81112bf78f686dc537dc48336c21
nagakumar2402 <nagakumar2402@gmail.com`>
2023-10-02 12:03:09 +0530

    lnk added

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -4,7 +4,7 @@ const Entry = () => {
   return (
     <section className="py-32 md:py-20">
       <div className="container flex flex-col items-center justify-center w-full px-4 mx-auto">
-        <div className="max-w-3xl mx-auto mb-16 text-center">
+        <div className="max-w-2xl mx-auto mb-16 text-center">
           <span className="font-bold tracking-widest text-green-700 uppercase">
             Pricing
           </span>
@@ -12,31 +12,31 @@ const Entry = () => {
             Zootopia Ticket Price
           </h2>
         </div>
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-          <div className="p-6 space-y-6 text-white bg-gray-900 rounded shadow sm:p-8 bg-img1">
+        <div className="grid w-full grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+          <div className="p-6 space-y-6 w-full text-white bg-gray-900 rounded shadow sm:p-8 bg-img1">
             <div className="space-y-2">
-              <h4 className="text-2xl font-bold">Zootopia</h4>
-              <span className="text-2xl font-bold md:text-3xl">
+              <h4 className="text-2xl px-4 font-bold">Zootopia</h4>
+              <span className="text-2xl px-4  font-bold md:text-3xl">
                 Entry charges
               </span>
             </div>
-            <div className="inline-block w-full px-5 py-1 text-xl font-semibold text-center border-2 border-green-700 rounded md:text-3xl bg-img">
+            <div className="inline-block  w-full px-5 py-1 text-xl font-semibold text-center border-2 border-green-700 rounded md:text-3xl bg-img">
               &#8377; 499 Rs
             </div>
           </div>
-          <div className="p-6 space-y-6 text-white bg-gray-900 rounded shadow sm:p-8 bg-img1">
+          <div className="p-6 space-y-6  w-full text-white bg-gray-900 rounded shadow sm:p-8 bg-img1">
             <div className="space-y-2">
-              <h4 className="text-2xl font-bold">animals</h4>
-              <span className="text-2xl font-bold md:text-3xl">Treat feed</span>
+              <h4 className="text-2xl  px-5 font-bold">animals</h4>
+              <span className="text-2xl px-5 font-bold md:text-3xl">Treat feed</span>
             </div>
             <div className="inline-block w-full px-5 py-1 text-xl font-semibold text-center border-2 border-green-700 rounded md:text-3xl bg-img">
               &#8377; 1250 Rs
             </div>
           </div>
-          <div className="p-6 space-y-6 text-white bg-gray-900 rounded shadow-2xl bg-img1 hover:scale-105 sm:p-8">
+          <div className="p-6 space-y-6  w-full text-white bg-gray-900 rounded shadow-2xl bg-img1 hover:scale-105 sm:p-8">
             <div className="space-y-2">
-              <h4 className="text-2xl font-bold">animals</h4>
-              <span className="text-2xl font-bold md:text-3xl">
+              <h4 className="text-2xl px-5 font-bold">animals</h4>
+              <span className="text-2xl px-5  font-bold md:text-3xl">
                 Feeding charges
               </span>
             </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
-import { IoChevronForward } from "../assets/Icons/index";
+
 import React from "react";
+import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
 import icon from "../assets/images/logo.png";
 const FooterOne = () => {
   return (
@@ -78,6 +79,29 @@ const FooterOne = () => {
                 </a>
               </li>
             </ul>
+          </div>
+        </div>
+        <div className="flex justify-center gap-4 md:justify-start">
+          <div
+            onClick={() => window.open("https://www.facebook.com/people/Zootopia-Funyard/pfbid02RW42Tr5AkqCbxcFNwxmQ229zwQcJcYFLZHvtswAgPesznF8ocFMLUyW7aLiMdPzBl/", "_blank")}
+            className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer"
+          >
+            <FaFacebookF size={20} />
+          </div>
+          <div
+          onClick={() => window.open("https://twitter.com", "_blank")}
+          className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+            <FaTwitter size={20} />
+          </div>
+          <div 
+          onClick={() => window.open("https://www.youtube.com/@Zootopia.funyard-by1ln/featured", "_blank")}
+          className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+            <FaYoutube size={20} />
+          </div>
+          <div 
+          onClick={() => window.open("https://www.instagram.com/zootopia.funyard/", "_blank")}
+          className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+            <FaInstagram size={20} />
           </div>
         </div>
       </div>
